f73f03afc3f446ef6c667231fb6a096a671d80f2
PatrickAndradeF <patrick.andrade@sou.unifal-mg.edu.br>
2021-10-28 21:58:25 -0300

    Impedir criar usuario sem cargo

--- fundecc atualizado/fundecc/src/main/java/projeto/controllers/UsuarioController.java
+++ fundecc atualizado/fundecc/src/main/java/projeto/controllers/UsuarioController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import projeto.Cargo;
 import projeto.Perfil;
 import projeto.PerfilRepositorio;
 import projeto.Usuario;
 import projeto.dto.PerfilDto;
 import projeto.dto.UsuarioDto;
+import projeto.service.CargoRepositorioDeMemoria;
 import projeto.service.PerfilRepositorioDeMemoria;
 import projeto.service.UsuarioRepositorioMemoria;
 
@@ -20,11 +22,13 @@ import java.util.List;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	private UsuarioRepositorioMemoria usuarioRepositorioMemoria;
-	PerfilRepositorio perfilRepositorio;
+	private PerfilRepositorio perfilRepositorio;
+	private CargoRepositorioDeMemoria cargoRepositorioDeMemoria;
 	
-	public UsuarioController(UsuarioRepositorioMemoria usuarioRepositorioMemoria, PerfilRepositorio perfilRepositorio){
+	public UsuarioController(UsuarioRepositorioMemoria usuarioRepositorioMemoria, PerfilRepositorio perfilRepositorio, CargoRepositorioDeMemoria cargoRepositorioDeMemoria){
 		this.usuarioRepositorioMemoria = usuarioRepositorioMemoria;
 		this.perfilRepositorio = perfilRepositorio;
+		this.cargoRepositorioDeMemoria  =cargoRepositorioDeMemoria;
 	}
 
 	@GetMapping
@@ -33,19 +37,24 @@ public class UsuarioController {
 	}
 	
 	@PostMapping
-	public String inserirCargos(@RequestBody UsuarioDto usuarioDto){
-		List<Perfil> perfis = new ArrayList<>();
-		List<Usuario> usuarios = this.usuarioRepositorioMemoria.buscaUsuario();
-		Usuario usuario = new Usuario(usuarioDto.getNome(), usuarioDto.getCpf(), usuarioDto.getCargo());
-		usuario.setSexo(usuarioDto.getSexo());
-		usuario.setDataNascimento(usuarioDto.getDataNascimento());
-		for(String i : usuarioDto.getPerfil()){
-			if(perfilRepositorio.buscaPerfilPorNome(i) != null){
-				perfis.add(perfilRepositorio.buscaPerfilPorNome(i));
+	public String inserirUsuarios(@RequestBody UsuarioDto usuarioDto){
+		Cargo cargo = new Cargo("");
+		cargo = cargoRepositorioDeMemoria.buscaCargoPorNome(usuarioDto.getCargo().getNome());
+		if(cargo != null){
+			List<Perfil> perfis = new ArrayList<>();
+			List<Usuario> usuarios = this.usuarioRepositorioMemoria.buscaUsuario();
+			Usuario usuario = new Usuario(usuarioDto.getNome(), usuarioDto.getCpf(), usuarioDto.getCargo());
+			usuario.setSexo(usuarioDto.getSexo());
+			usuario.setDataNascimento(usuarioDto.getDataNascimento());
+			for(String i : usuarioDto.getPerfil()){
+				if(perfilRepositorio.buscaPerfilPorNome(i) != null){
+					perfis.add(perfilRepositorio.buscaPerfilPorNome(i));
+				}
 			}
+			usuario.setPerfil(perfis);
+			this.usuarioRepositorioMemoria.cadastraUsuario(usuario, usuarios);
+			return usuario.getNome() + " Inserido com sucesso";
 		}
-		usuario.setPerfil(perfis);
-		this.usuarioRepositorioMemoria.cadastraUsuario(usuario, usuarios);
-		return usuario.getNome() + " Inserido com sucesso";
+		return "Usuario não cadastrado";
 	}
 }
